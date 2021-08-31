f8db5ae34af5bbb8afe4f6cd87357e1972595f34
supremoods <80272399+supremoods@users.noreply.github.com>
2021-08-31 20:13:29 +0800

    updated forgot

--- supRemood/forgot.php
+++ supRemood/forgot.php
@@ -4,8 +4,10 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ALPAS | Forgot</title>
+  <link rel="icon" href="images/logo.png" />
   <link rel="stylesheet" type="text/css" href="assets/css/forgot.css">
-  <link rel="stylesheet" href="assets/css/nav.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/nav.css">
   <link rel="stylesheet" type="text/css" href="assets/css/toast.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 </head>
@@ -14,7 +16,7 @@
   <div class="header">
     <nav class="nav-bar">
       <div class="content">
-        <div class="logo"><a href="#">ALPAS</a></div>
+        <div class="logo"><a href="#"><img src="images/logo.png" alt=""><p>ALPAS</p></a></div>
         <ul class="menu-list">
           <div class="icon cancel-btn">
             <i class="fas fa-times"></i>
@@ -32,24 +34,29 @@
     </nav>
   </div>
 
-  <div class="background">
-    <form class="form" method="POST">
-    <p>RESET PASSWORD</p><br><br>
-      <div class="pass">
-        <input type="email" class="textplace-1" id="email" placeholder="Email" name="email" required><br><br>
-        <!--
-        <label> New Password</label><br>
-        <input class="textplace" id="password" type="password" placeholder=" password">
-        <br><br>
-        <label> Re-enter Password</label><br>
-        <input class="textplace" id="co_password" type="password" placeholder="Re-enter password"><br>
-        -->
-        <div class="RegisterButton">
-         <input id="submit-button" type="submit" value="Proceed" name="submit"></input>
-        </div>
+  <div class="forgot-container">
+    <div class="background">
+      <div class="bg-cont">
+        <form class="form" method="POST">
+          <p>Reset Password</p><br><br>
+            <div class="pass">
+              <input type="email" class="textplace-1" id="email" placeholder="Email" name="email" required><br><br>
+              <!--
+              <label> New Password</label><br>
+              <input class="textplace" id="password" type="password" placeholder=" password">
+              <br><br>
+              <label> Re-enter Password</label><br>
+              <input class="textplace" id="co_password" type="password" placeholder="Re-enter password"><br>
+              -->
+              <div class="RegisterButton">
+              <input id="submit-button" type="submit" value="Proceed" name="submit"></input>
+              </div>
+            </div>
+        </form>
       </div>
-    </form>
+    </div>
   </div>
+  
   <section class="footer">
     <div class="social">
       <a href="http://"><i class="fab fa-instagram"></i></a>
